fff929d04b7d8750c155fcc5c4aa9a0dd4639359
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-08-30 07:42:49 +0300

    refactor: Update counterReducer function to fix action parameter order

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 import classes from './Counter.module.css';
 import { useDispatch, useSelector } from 'react-redux'
-
+import { counterActions } from '../store/index'
 
 const Counter = () => {
   //subscription
@@ -11,17 +11,17 @@ const Counter = () => {
   const dispatch = useDispatch();
   //dispatch functions
   const incrementHandler = () => {
-    dispatch({ type: "increment", amount: 1 })
+    dispatch(counterActions.increment())
   }
   const incrementHandlerBy5 = () => {
-    dispatch({ type: "increment", amount: 5 })
+    dispatch(counterActions.increase(5))
   }
   const decrementHandler = () => {
-    dispatch({ type: "decrement" })
+    dispatch(counterActions.decrement())
   }
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' })
+    dispatch(counterActions.toggleCounter())
   };
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,35 @@
-import { createStore } from 'redux'
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true }
-//forwarded to
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
+
+//1-every slice need a name
+//2- insitial State
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    //reducers are functions that can update the state
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
         }
     }
-    if (action.type === 'decrement') {
-        return {
-            showCounter: state.showCounter,
-            counter: state.counter - 1
-        }
-    }
+})
 
-    if (action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-    return state;
-}
 
-//Store ( change datat in store)
-const store = createStore(counterReducer);
 
+//Store is created by passing the reducer to the configureStore function
+const store = configureStore({
+    reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
 export default store;
